8f8b020b2876ee9e35051d2464cb434b4fc75712
lbx <837944603@qq.com>
2018-03-30 17:48:10 +0800

    拦截器

---
+++ web/src/main/java/com/lbx/web/Interceptor/SqlInjectInterceptor.java
@@ -0,0 +1,92 @@
+package com.lbx.web.Interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+
+/**
+ * @Author:
+ * @date 2018/3/30 17:42
+ */
+public class SqlInjectInterceptor implements HandlerInterceptor {
+
+
+    /**
+     * 处理字符转义 * * @param value * @return
+     */
+    private String clearXss(String value) {
+        if (value == null || "".equals(value)) {
+            return value;
+        }
+        value = value.replaceAll("<", "<").replaceAll(">", ">");
+        value = value.replaceAll("\\(", "(").replace("\\)", ")");
+        value = value.replaceAll("'", "'");
+        value = value.replaceAll("eval\\((.*)\\)", "");
+        value = value.replaceAll("[\\\"\\\'][\\s]*javascript:(.*)[\\\"\\\']", "\"\"");
+        value = value.replace("script", "");
+        return value;
+    }
+
+
+
+
+    @Override
+
+    public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
+            throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
+            throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2) throws Exception {
+        // TODO Auto-generated method stub
+        if (arg0.getRequestURI().indexOf("CheckAcc") > -1) {
+            Enumeration<String> names = arg0.getParameterNames();
+            while (names.hasMoreElements()) {
+                String name = names.nextElement();
+                String[] values = arg0.getParameterValues(name);
+                for (String value : values) {
+                    if (judgeXSS(value.toLowerCase())) {
+                        arg1.setContentType("text/html;charset=UTF-8");
+                        arg1.getWriter().print("参数含有非法攻击字符,已禁止继续访问！");
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 判断参数是否含有攻击串
+     *
+     * @param value
+     * @return
+     */
+    public boolean judgeXSS(String value) {
+        if (value == null || "".equals(value)) {
+            return false;
+        }
+        String xssStr = "and|or|select|update|delete|drop|truncate|%20|=|-|--|;|'|%|#|+|,|//|/| |\\|!=|(|)";
+        String[] xssArr = xssStr.split("\\|");
+        for (int i = 0; i < xssArr.length; i++) {
+            if (value.indexOf(xssArr[i]) > -1) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
--- web/src/main/java/com/lbx/web/controller/IndexController.java
+++ web/src/main/java/com/lbx/web/controller/IndexController.java
@@ -2,7 +2,6 @@ package com.lbx.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Create by lbx on 2018/3/11  17:44
